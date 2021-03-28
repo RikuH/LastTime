ef997d4fc66c696153d30a6ec19857f4dc56c1ac
RikuH <45202322+RikuH@users.noreply.github.com>
2021-03-28 02:05:24 +0200

    Muaailma pieni paikka on

---
+++ ToimiNyt/Assets/Scripts/DeathPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeathPanel : MonoBehaviour
+{
+    public GameObject panel;
+    public Button batotomenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        batotomenu.onClick.AddListener(backtomenumeth);
+    }
+
+    void backtomenumeth()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
--- ToimiNyt/Assets/Scripts/FadeScript.cs
+++ ToimiNyt/Assets/Scripts/FadeScript.cs
@@ -8,7 +8,6 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class FadeScript : MonoBehaviour
 {
-    public Image endImage;
     public Image startImage;
     public Animator anim;
     void Start()
@@ -17,26 +16,4 @@ public class FadeScript : MonoBehaviour
         anim = GetComponent<Animator>();
         anim.SetBool("StartFadeToGame", true);
     }
-
-    private void Update()
-    {
-    }
-
-    IEnumerator PrologyTimer()
-    {
-        yield return new WaitForSeconds(23);
-        //player.gameObject.SetActive(true);
-        //animationGirl.SetActive(false);
-        //BedroomLight.SetActive(true);
-        //prologyIsOver = true;
-        //player.agent.enabled = true;
-    }
-
-    public IEnumerator EndGame()
-    {
-        yield return new WaitForSeconds(2);
-        anim.SetBool("EndImageFade", true);
-        //SceneManager.LoadScene("MainMenu");
-    }
-
 }
